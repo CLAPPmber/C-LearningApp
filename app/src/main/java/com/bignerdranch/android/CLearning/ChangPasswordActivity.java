994b148c9804348f0694133c1e2ff2e284f2a01a
susurong <854591549@qq.com>
2018-09-16 11:12:33 +0800

    susu

---
+++ app/src/main/java/com/bignerdranch/android/CLearning/ChangPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.CLearning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chang_password);
+    }
+}
