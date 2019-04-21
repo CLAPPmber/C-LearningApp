eb1e2832f7489b0138d8643eb065f70427c1a0ed
WindSnowSky <185904641@qq.com>
2019-04-21 17:35:30 +0800

    习题系统性能优化

--- app/src/main/java/com/bignerdranch/android/CLearning/ExerciseSystem.java
+++ app/src/main/java/com/bignerdranch/android/CLearning/ExerciseSystem.java
@@ -77,11 +77,11 @@ public class ExerciseSystem extends Fragment {
             @Override
             public void onClick(View v) {
                 flag = 0;
-                set_test_progress(0);   //用户做题进度更新
-                if(key0) {
+//                set_test_progress(0);   //用户做题进度更新
+//                if(key0) {
                     Intent intent = new Intent(getActivity(), Practice.class);
                     startActivity(intent);
-                }
+//                }
             }
         });
         //添加button2(试题训练),并设置图片的大小
@@ -93,11 +93,11 @@ public class ExerciseSystem extends Fragment {
             @Override
             public void onClick(View v) {
                 flag = 1;
-                set_test_progress(1);   //用户做题进度更新
-                if(key1) {
+//                set_test_progress(1);   //用户做题进度更新
+//                if(key1) {
                     Intent intent = new Intent(getActivity(), Practice.class);
                     startActivity(intent);
-                }
+//                }
             }
         });
         //添加button3(社区答疑),并设置图片的大小
@@ -152,7 +152,7 @@ public class ExerciseSystem extends Fragment {
             @Override
             public void onFailure(int code, String msg) {//操作错误
                 Log.e("err",msg);
-                Toast.makeText(getActivity(), "网络异常", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getActivity(), "网络异常", Toast.LENGTH_SHORT).show();
 
             }
         });
